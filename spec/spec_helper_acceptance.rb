0e37779c23818f6a1fa441efe1fa54037ef5c382
tphoney <tp@puppet.com>
2017-09-26 10:57:25 +0100

    Cleanup via rubocop of ruby code

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -15,7 +15,7 @@ install_bolt_on(hosts) unless pe_install?
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
-UNSUPPORTED_PLATFORMS = ['RedHat','Suse','windows','AIX','Solaris']
+UNSUPPORTED_PLATFORMS = %w[RedHat Suse windows AIX Solaris].freeze
 
 DEFAULT_PASSWORD = if default[:hypervisor] == 'vagrant'
                      'vagrant'
@@ -70,7 +70,7 @@ def retry_on_error_matching(max_retry_count = 3, retry_wait_interval_secs = 5, e
     try += 1
     yield
   rescue Exception => e
-    if try < max_retry_count && (error_matcher == nil || e.message =~ error_matcher)
+    if try < max_retry_count && (error_matcher.nil? || e.message =~ error_matcher)
       sleep retry_wait_interval_secs
       retry
     else
